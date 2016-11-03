a2db00b618bda8256ad4ab2b25708b774e112e32
Zer0dot <zodiak90yt@gmail.com>
2016-11-02 21:58:27 -0400

    Added first project
    First C# exclusive project added

---
+++ 2D_Test_01/Assets/Scripts/UserControls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserControls : MonoBehaviour {
+	//public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-15, 0));
+		} 
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (15, 0));
+		}
+	}
+}
